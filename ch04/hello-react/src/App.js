252d7685cb32251f09f562a67c75967861d1bd64
jaejun-ko <goj609@gmail.com>
2021-01-25 21:35:03 +0900

    * modules  - ch04  - ch05  - ch06  - ch07

---
+++ ch04/hello-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import EventPractice from './EventPractice';
+// import EventPractice2 from './EventPractice2';
+// import EventPractice3 from './EventPractice3';
+import EventPractice4 from './EventPractice4';
+
+const App = () => {
+  // return <EventPractice />;
+  // return <EventPractice2 />;
+  // return <EventPractice3 />;
+  return <EventPractice4 />;
+};
+
+export default App;
---
+++ ch06/hello-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import IterationSample from './IterationSample';
+
+function App() {
+  return (
+    <div>
+      <IterationSample />
+    </div>
+  );
+}
+
+export default App;
